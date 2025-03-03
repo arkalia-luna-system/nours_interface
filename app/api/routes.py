6b7d19b20f858252e9f66f294d679c4ef3aa3897
TonNomGitHub <TonEmailGitHub>
2025-03-03 01:57:35 +0100

    Fix images et redémarrage automatique

--- app/api/routes.py
+++ app/api/routes.py
@@ -6,5 +6,5 @@ api_router = APIRouter()
 
 @api_router.get("/nours-image")
 async def get_nours_image():
-
-    image_path = os.path.join("static", "images", "nours.png")
+    image_path = os.path.join(os.getcwd(), "static", "images", "nours.png")
+    return FileResponse(image_path)
